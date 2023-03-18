7dfdc87ca9172a7d8b5b132f3e55d6e60c4df160
Pandjiimansp001 <pandjiimansp001@gmail.com>
2023-03-18 08:46:25 +0700

    update

--- src/components/Portfolio/FrontEnd/projectData.js
+++ src/components/Portfolio/FrontEnd/projectData.js
@@ -8,32 +8,28 @@ const projectData = [
     id: "1",
     image: gaweyoPortfolio,
     name: "Gaweyo Project Portfolio",
-    description:
-      "At refourma, I did an internship as a front end web developer for 3 months. I managed to complete the 1 month React js programming training provided by the company and after that I started working on real projects. I also managed to complete 50+ work tasks for company projects well. The technologies that I use include reactjs, tailwind css, and the use of git in working on the project.",
+    description: "Build with: React Js, Tailwind CSS",
     link: "https://gaweyo-portfolio.vercel.app/",
   },
   {
     id: "2",
     image: todoApp,
     name: "React Todo App",
-    description:
-      "Project build with reactjs and tailwind css. In the todo app application there are several functions such as adding data and also deleting data by clicking on an existing task",
+    description: "Build with: React Js, Tailwind CSS",
     link: "https://react-todo-app-two-blush.vercel.app/",
   },
   {
     id: "3",
     image: expenseTracker,
     name: "React Expense Tracker App",
-    description:
-      "The expense tracker app consists of several functions or components including add and filter which are equipped with bar charts. this app build with reactjs and tailwind css",
+    description: "Build with: React Js, Tailwind CSS",
     link: "https://react-expense-tracker-chi.vercel.app/",
   },
   {
     id: "4",
     image: secondmart,
     name: "React E-Commerce Secondmart",
-    description:
-      "The components in the project consist of login and registration pages with authentication. checkout, shop, product details and cart pages",
+    description: "Build with: React Js, Bootstrap CSS, Firebase/Firestore",
     link: "https://ecommerce-secondmart.vercel.app/",
   },
 ];
